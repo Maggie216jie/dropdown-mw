5173828929c583556c8fa0adee7c250a3bd9b4d0
Mengjie Wang <mw3518@nyu.edu>
2021-10-19 12:28:27 -0500

    changes

--- dropdown-selections/src/App.js
+++ dropdown-selections/src/App.js
@@ -4,13 +4,25 @@ import '../node_modules/font-awesome/css/font-awesome.min.css';
 import DropdownSelection from './Dropdown/DropdownSelection';
 
 function App() {
+
+  let onChange =(value)=>{
+
+    console.log(value)
+
+  }
+
+
   return (
     <div>
       <DropdownSelection 
         //selectType has two values: "single" and "mulitiple". Mulitiple is the default one.
-        selectType ={"single"}
+        selectType ={"mulitiple"}
         //option
         options= {[{value:"apple", lable:"Apple"},{value:"orange", lable:"Orange"}, {value:"banana", lable:"Banana"}]}
+        // for "single" only one value
+        preSelect ={["apple","orange"]}
+        onChange = {onChange}
+
       />
     </div>
   );
--- dropdown-selections/src/Dropdown/DropdownSelection.js
+++ dropdown-selections/src/Dropdown/DropdownSelection.js
@@ -10,8 +10,56 @@ class DropdownSelection extends Component {
           selected: "test",
           single:{
 
-          }
+          },
+          checked:{}
         };
+    }
+
+    componentDidMount(){
+
+      this.getPreSelected()
+
+
+    }
+
+    getPreSelected = () =>{
+      let preObject ={}
+      let allfalse ={}
+
+
+      if(this.props.selectType==="single"){
+
+        for(const op of this.props.options){
+          allfalse[op.value] = false
+          if(op.value===this.props.preSelect[0]){
+            preObject[this.props.preSelect[0]]=true
+          }else{
+            preObject[op.value]=false
+
+
+          }
+
+        }
+
+      
+
+      }else{
+        for(const pre of this.props.preSelect){
+          preObject[pre]=true
+      
+        }
+  
+
+      }
+
+      this.setState({
+        checked:{
+          ...this.state.checked,
+          ...preObject
+        },
+        allfalse:allfalse
+      })
+
     }
 
     toggleDropdown = () =>{
@@ -19,15 +67,79 @@ class DropdownSelection extends Component {
             openDropdown: !this.state.openDropdown
         })
     }
+
+    onChangeHandler = (e) =>{
+
+      if(this.props.selectType==="single"){
+        if(e.target.checked&&this.state.checked[e.target.value]){
+          
+          this.setState({
+            checked:{
+             ...this.state.allfalse,
+           
+            }
+          })
+
+        }else{
+          this.setState({
+            checked:{
+              ...this.state.allfalse,
+              [e.target.value]: e.target.checked
+            }
+          })
+        }
+      }else{
+        this.setState({
+          checked:{
+            ...this.state.checked,
+            [e.target.value]: e.target.checked
+          }
+        })
+
+      }
+
+      setTimeout(()=>{   this.props.onChange(this.getCheckedValue())},100)
+
+  
+
+    }
+
+    getCheckedValue = () =>{
+
+      let result = []
+
+      let original = JSON.parse(JSON.stringify(this.state.checked))
+      for(let i in original){
+        if(original[i]){
+
+          result.push(i)
+
+        }
+
+      }
+      return result
+
+
+    }
     renderOptions = () =>{
 
         let type = this.props.selectType==="single"? "radio": "checkbox"
 
         return this.props.options.map((item, i) =>{
+
+  
             return (
            
                 <div className="dropdown-item" key={item.value}>
-                    <input type={type} id={item.value} name={type==="radio"?"slection":item.value} value={item.value}/>
+                    <input 
+                      type={type} 
+                      id={item.value} 
+                    
+                      name={item.valye} 
+                      value={item.value} 
+                      checked={this.state.checked[item.value]}
+                      onClick={this.onChangeHandler}
+                    />
                     <label htmlFor={item.value}> {item.lable}</label>
                 </div>
         
@@ -45,7 +157,7 @@ class DropdownSelection extends Component {
               onClick={() => this.toggleDropdown()}
               className="dropdown-toggle"
             >
-              {this.state.selected}
+              {this.getCheckedValue().join(", ") || "-- Please Select --"}
               {this.state.openDropdown?
                 <i className="fa fa-angle-up" aria-hidden="true"></i>
                 :
